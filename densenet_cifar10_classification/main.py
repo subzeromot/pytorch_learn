cb67154accf61e991437ce6da2e9e6ccb28b9ee6
subzero <subzeromot@gmail.com>
2021-01-30 01:46:20 +0700

    refactor

--- densenet_cifar10_classification/main.py
+++ densenet_cifar10_classification/main.py
@@ -9,112 +9,122 @@ import matplotlib.pyplot as plt
 import numpy as np
 from densenet import densenet_cifar
 
-# Transform function
-transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+class Process:
+    def __init__(self):
+        # Transform function
+        self.transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
-# Load dataset
-train_set = torchvision.datasets.CIFAR10('./datasets', train=True, download=True, transform=transform)
-test_set = torchvision.datasets.CIFAR10('./datasets', train=True, download=False, transform=transform)
+        # Load dataset
+        classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        train_set = torchvision.datasets.CIFAR10('./datasets', train=True, download=True, transform=self.transform)
+        test_set = torchvision.datasets.CIFAR10('./datasets', train=True, download=False, transform=self.transform)
+        self.train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=4)
+        self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=False, num_workers=4)
 
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True, num_workers=4)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=128, shuffle=False, num_workers=4)
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        print(self.device)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        # Load NET
+        self.net = densenet_cifar().to(self.device)
+        # print(net)
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-print(device)
+        if self.device == 'cuda':
+            self.net = nn.DataParallel(self.net)
+            torch.backends.cudnn.benchmark = True
 
-net = densenet_cifar().to(device)
-# print(net)
+        lr = 0.01 #1e-1
+        momentum = 0.9
+        weight_decay = 1e-4
 
-if device == 'cuda':
-    net = nn.DataParallel(net)
-    torch.backends.cudnn.benchmark = True
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(self.net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[150, 225])
+    
+    def scheduler_step(self):
+        self.scheduler.step()
 
+    # Training
+    def train(self, epoch):
+        self.net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+        for batch_idx, (inputs, targets) in enumerate(self.train_loader):
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            self.optimizer.zero_grad()
+            outputs = self.net(inputs)
+            loss = self.criterion(outputs, targets)
+            loss.backward()
+            self.optimizer.step()
 
-lr = 0.01 #1e-1
-momentum = 0.9
-weight_decay = 1e-4
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 225])
-
-# Training
-def train(epoch):
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        print("Epoch:" , epoch, " ========= loss: ", loss)
-    acc = 100.*correct/total
-    loss = train_loss / batch_idx
-    print('Epoch: %d, train loss: %.6f, acc: %.3f%% (%d/%d)' % (epoch, loss, acc, correct, total))
-    return loss
-
-def test(epoch):
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(test_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-
-            test_loss += loss.item()
+            train_loss += loss.item()
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-    acc = 100.*correct/total
-    loss = test_loss / batch_idx
-    print('Epoch: %d, test loss: %.6f, acc: %.3f%% (%d/%d)' % (epoch, loss, acc, correct, total))
-    return loss
 
+            print("Epoch:" , epoch, " ========= loss: ", loss)
+        acc = 100.*correct/total
+        loss = train_loss / batch_idx
+        print('Epoch: %d, train loss: %.6f, acc: %.3f%% (%d/%d)' % (epoch, loss, acc, correct, total))
+        return loss
 
+    def test(self, epoch):
+        self.net.eval()
+        test_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(self.test_loader):
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                outputs = self.net(inputs)
+                loss = self.criterion(outputs, targets)
 
-load_model = False
-if load_model:
-    checkpoint = torch.load('./checkpoint/densenet.ckpt')
-    net.load_state_dict(checkpoint['net'])
-    start_epoch = checkpoint['epoch']
-else:
-    start_epoch = 0
-print('start_epoch: %s' % start_epoch)
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+        acc = 100.*correct/total
+        loss = test_loss / batch_idx
+        print('Epoch: %d, test loss: %.6f, acc: %.3f%% (%d/%d)' % (epoch, loss, acc, correct, total))
+        return loss
 
-def plot_loss(train_losses, test_losses):
-    plt.plot(range(len(train_losses)), train_losses)
-    plt.plot(range(len(test_losses)), test_losses)
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.show()
+    def plot_loss(self, train_losses, test_losses):
+        print(train_losses)
+        print(test_losses)
+        plt.plot(range(len(train_losses)), train_losses)
+        plt.plot(range(len(test_losses)), test_losses)
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        # plt.show()
+        plt.draw()
+        plt.savefig('result.png')
 
-train_losses = []
-test_losses = []
+def main():
+    process = Process()
+    load_model = False
+    if load_model:
+        checkpoint = torch.load('./checkpoint/densenet.ckpt')
+        net.load_state_dict(checkpoint['net'])
+        start_epoch = checkpoint['epoch']
+    else:
+        start_epoch = 0
+    print('start_epoch: %s' % start_epoch)
 
-for epoch in range(start_epoch, 1):
-    scheduler.step()
-    train_loss = train(epoch)
-    test_loss = test(epoch)
-    train_losses.append(train_loss)
-    test_losses.append(test_loss)
-    start_epoch = epoch
+    train_losses = []
+    test_losses = []
 
+    for epoch in range(start_epoch, 1):
+        # scheduler.step()
+        process.scheduler_step()
+        train_loss = process.train(epoch)
+        test_loss = process.test(epoch)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        start_epoch = epoch
+
+if __name__ == "__main__":
+    main()
